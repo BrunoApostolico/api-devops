11bd3ffa5cb01beaf5cca86a60fe95ff61b07697
Bruno Apostolico <bapostolico@gmail.com>
2023-04-02 00:23:05 -0300

    Initial commit

---
+++ src/main/java/infnet/edu/apidevops/ApiDevopsApplication.java
@@ -0,0 +1,13 @@
+package infnet.edu.apidevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDevopsApplication.class, args);
+	}
+
+}
